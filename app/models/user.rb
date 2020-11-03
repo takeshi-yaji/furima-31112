de22f9f7542318fb8d9248251de6487de7b72c99
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-03 17:33:21 +0900

    usersテーブルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+
+  with_options presence: true do
+  validates :nickname
+  validates :first_name
+  validates :last_name
+  validates :first_name_kana
+  validates :last_name_kana
+  validates :birthday
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
