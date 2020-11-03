79c4c1f6bb3a07fccff30905fc7d909fc69bdbc7
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-03 17:34:51 +0900

    userモデルの単体テストコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { "t000000" }
     password_confirmation { password }
     last_name             { '斎藤' }
     first_name            { '一' }
