1f332eaf42e8cab120dee2ae6603788417e9afc2
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-04 10:16:09 +0900

    画像表示の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :buyer
+  has_one_attached :image
+
+  with_options presence: true do
+  validates :name
+  validates :content
+  validates :category_id
+  validates :status_id
+  validates :charges_id
+  validates :prefecture_id
+  validates :price
+  validates :day_id
+  end
 end
