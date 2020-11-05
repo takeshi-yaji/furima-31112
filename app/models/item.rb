c183573034d5b1b5344954acdb03c34e0c46ec70
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-05 17:57:27 +0900

    itemモデルのアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :day
   belongs_to :user
   has_one :buyer
   has_one_attached :image
