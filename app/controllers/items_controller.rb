067603f37ef909333dd07bf9e5a536b895922e6a
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-07 16:55:01 +0900

    商品削除機能のコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+     if current_user.id == @item.user_id 
       @item.destroy
       redirect_to action: :index
+     end
   end
 
   private
