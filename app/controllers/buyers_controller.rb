ee6c62c3e58329485b69d280d2a90f8c1a394642
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-11-13 20:58:01 +0900

    購入機能のコントローラー修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,7 +1,7 @@
 class BuyersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_index, only: [:index]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @buyer_form = BuyerForm.new
